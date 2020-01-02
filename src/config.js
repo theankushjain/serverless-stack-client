cbd249ca3176e462f86855b9a1f690df0179c85f
JAIN Ankush <ankush.jain@soprasteria.com>
2020-01-02 17:38:31 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-sopra"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://dvd0d7sw2i.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_Z3LZdoCyE",
+      APP_CLIENT_ID: "4729mk9o6t6a7p93nukf4tshl0",
+      IDENTITY_POOL_ID: "us-east-1:e13ac5f1-5d6a-478f-b71f-3d2ee93b5ea0"
+    }
+  };
